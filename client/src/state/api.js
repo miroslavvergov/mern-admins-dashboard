9f2a155a56412bd979d4cd777f503ff64f9b1c2d
miroslavvergov <gabberlt123@gmail.com>
2024-03-13 23:10:39 +0200

    first commit

---
+++ client/src/state/api.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admins",
+    "Performance",
+    "Dashboard",
+  ],
+  endpoints: (build) => ({
+    
+  }),
+});
+
+export const {
+  
+} = api;
